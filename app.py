bb970c37bb2c057bba66ae3a2c96ba43eb9351ab
Daranee <143437590+dbala12@users.noreply.github.com>
2026-03-23 19:49:28 +0000

    start app.py

--- app.py
+++ app.py
@@ -3,5 +3,3 @@ import folium
 from geopy.geocoders import Nominatim
 from geopy.extra.rate.limiter import RateLimiter
 
-# SETUP
-file_path = 'https://docs.google.com/spreadsheets/d/1Myzcaf08GQzNApCD_gnoi4e60GlRiWLbCaXVfZiEfoU/edit?gid=874322643#gid=874322643'
